1a8c01d4590f8445694a274cd127dd6fdb778269
JBRS307 <kuba.rekas.307@gmail.com>
2023-03-11 13:20:17 +0100

    heapsort finished

--- algorithms/sort/heapsort.py
+++ algorithms/sort/heapsort.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 #funkcje pomocnicze
 def left(i):
     return 2*i+1
@@ -10,9 +12,7 @@ def parent(i):
 
 
 #Przywracanie własności kopca
-def heapify(arr, i):
-    n = len(arr)
-
+def heapify(arr, i, n):
     l = left(i)
     r = right(i)
     max_index = i
@@ -24,12 +24,30 @@ def heapify(arr, i):
     
     if max_index != i:
         arr[i], arr[max_index] = arr[max_index], arr[i]
-        heapify(arr, max_index)
+        heapify(arr, max_index, n)
     return
 
 #Budowanie kopca
-def build_heap(arr):
-    n = len(arr)
-
+def build_heap(arr, n):
     for i in range(parent(n-1), -1, -1):
-        heapify(arr, i)
+        heapify(arr, i, n)
+    
+    return
+
+#Heaposort
+def heapsort(arr):
+    n = len(arr)
+    build_heap(arr, n)
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, 0, i)
+    return
+
+if __name__ == "__main__":
+    n = 30
+    k = 100
+    arr = [randrange(1, k+1) for _ in range(n)]
+
+    print(*arr)
+    heapsort(arr)
+    print(*arr)
