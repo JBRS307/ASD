bc9ce118e5988132391edc1c45e6e5c5f5d12632
JBRS307 <kuba.rekas.307@gmail.com>
2023-03-18 15:23:33 +0100

    mergesort updated

--- algorithms/sort/mergesort_best.py
+++ algorithms/sort/mergesort_best.py
@@ -1,18 +1,13 @@
 from random import randrange
 from time import time
 
-#Merge Sort
-def merge(arr, l, m, r): #l - start 1, r - start2
+def merge(arr, l, m, r):
     n1 = m-l+1
     n2 = r-m
-    arr1 = [0]*(n1)
-    arr2 = [0]*(n2)
-
-    for i in range(n1):
-        arr1[i] = arr[l+i]
-    for i in range(n2):
-        arr2[i] = arr[m+1+i]
     
+    arr1 = arr[l:m+1]
+    arr2 = arr[m+1:r+1]
+
     i1 = i2 = 0
     j = l
     while i1 < n1 and i2 < n2:
@@ -39,15 +34,13 @@ def mergesort(arr, l, r):
         mergesort(arr, l, m)
         mergesort(arr, m+1, r)
         merge(arr, l, m, r)
-#End Merge Sort
 
 if __name__ == "__main__":
     arr = [randrange(1, 101) for _ in range(1_000_000)]
     print("Done", end="\n\n")
-    
-    start_time = time()
-    mergesort(arr, 0, 999_999)
-    end_time = time()
 
-    print(end_time-start_time)
-    # print(*arr)
+    start_time = time()
+    arr = mergesort(arr, 0, 999_999)
+    end_time = time()
+    
+    print(end_time - start_time)
