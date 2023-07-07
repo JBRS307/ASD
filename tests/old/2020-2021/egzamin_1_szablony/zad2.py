03d7c32d96e0fa76216cce36ab4aa9141d214281
JBRS307 <kuba.rekas.307@gmail.com>
2023-07-07 13:39:03 +0200

    some 2020-2021 done

--- tests/old/2020-2021/egzamin_1_szablony/zad2.py
+++ tests/old/2020-2021/egzamin_1_szablony/zad2.py
@@ -1,10 +1,28 @@
 from zad2testy import runtests
+from queue import PriorityQueue
 
 
 def robot( L, A, B ):
-    """tu prosze wpisac wlasna implementacje"""
-    return 0
+    dp = [[[[-1]*3 for _ in range(4)] for _ in range(len(L[0]))] for _ in range(len(L))]
+    possible_moves = ((1, 0), (0, 1), (-1, 0), (0, -1))
+    seconds = (60, 40, 30)
+    q = PriorityQueue()
+    q.put((0, A[0], A[1], 0, 0))
 
+    while not q.empty():
+        time, x, y, direction, speed = q.get()
+        if (x, y) == B:
+            return time
+        if dp[y][x][direction][speed] != -1 or L[y][x] == 'X':
+            continue
+        dp[y][x][direction][speed] = time
+        q.put((time+45, x, y, (direction+1)%4, 0))
+        q.put((time+45, x, y, (direction+3)%4, 0))
+        x += possible_moves[direction][0]
+        y += possible_moves[direction][1]
+        q.put((time+seconds[speed], x, y, direction, min(speed+1, 2)))
+
+    return None
     
 runtests( robot )
 
--- tests/old/2020-2021/egzamin_2_szablony/zad1.py
+++ tests/old/2020-2021/egzamin_2_szablony/zad1.py
@@ -2,7 +2,12 @@ from zad1testy import runtests
 
 
 def intuse( I, x, y ):
-    """tu prosze wpisac wlasna implementacje"""
+    n = len(I)
+    for i in range(n):
+        I[i] = (i, I[0], I[1])
+    
+
+
     return []
 
     
